7ae70a79e7a7f22436ba7d6b6db5b5f31d0653f6
Peter Edwards <peadar@arista.com>
2023-12-04 11:32:03 +0000

    denser

--- 04/solve.py
+++ 04/solve.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-
 import sys
-
 with open(sys.argv[1]) as f:
-    def parseLine(l):
-        _, vals = [s.strip() for s in l.split(":")]
-        sets = [ frozenset(int(i) for i in side.split())
-                for side in (s.strip() for s in vals.split("|")) ]
-        return len( frozenset.intersection( *sets ) )
-    cards = [ parseLine(datum) for datum in f ]
-
-print(sum(1 << (isec-1) for isec in cards if isec != 0))
-
-counts = [ 1 ] * len(cards)
-for i, isec in enumerate(cards):
+    winnerCounts = [ len(frozenset.intersection(*
+            (frozenset(int(i) for i in side.split())
+                for side in (s.strip() for s in
+                [s.strip() for s in l.split(":")][1]
+                .split("|")))))
+        for l in f ]
+print(sum(1 << (isec-1) for isec in winnerCounts if isec != 0))
+counts = [ 1 ] * len(winnerCounts)
+for i, isec in enumerate(winnerCounts):
     for j in range(i + 1, i+1+isec):
         counts[j]+=counts[i]
 print(sum(counts))

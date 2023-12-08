60e1b3bcf7770895b036d87cc8f14c0ea7e2939c
Peter Edwards <peadar@arista.com>
2023-12-08 01:18:51 +0000

    I think this is a bit more succinct

--- 07/solve.cc
+++ 07/solve.cc
@@ -5,9 +5,9 @@
 
 using ulong = unsigned long;
 enum class HandType { HIGH, PAIR, TWOPAIR, THREE, HOUSE, FOUR, FIVE, };
-enum class Card { BAD, WILD, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE };
+enum class Card { BAD, JOKER, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE };
 
-template <bool wild> struct Hand {
+template <Card jcard> struct Hand {
    HandType type;
    std::array<Card, 5> cards {};
    ulong bid {};
@@ -15,32 +15,30 @@ template <bool wild> struct Hand {
    auto operator <=> (const Hand &r) const = default;
 };
 
-std::istream & operator >> (std::istream &is, Card &c) {
+template <Card jcard> Card
+parseCard(std::istream &is) {
    char ch;
    is >> ch;
    switch (ch) {
-      case '2'...'9': c = Card(int(Card::TWO) + ch - '2'); break;
-      case 'T': c = Card::TEN; break;
-      case 'J': c = Card::JACK; break;
-      case 'Q': c = Card::QUEEN; break;
-      case 'K': c = Card::KING; break;
-      case 'A': c = Card::ACE; break;
-      default: c = Card::BAD;
+      case '2'...'9': return Card(int(Card::TWO) + ch - '2');
+      case 'T': return Card::TEN;
+      case 'J': return jcard;
+      case 'Q': return Card::QUEEN;
+      case 'K': return Card::KING;
+      case 'A': return Card::ACE;
+      default: return Card::BAD;
    }
-   return is;
 }
 
 // parse (and classify a hand)
-template<bool wild> Hand<wild>::Hand(std::istream &is) {
+template<Card j> Hand<j>::Hand(std::istream &is) {
    std::unordered_map<Card, int> counts;
    std::array<char, 6> ofkind {};
-   int jacks = 0;
+   int jokers = 0;
    for (int i = 0; i < 5; ++i) {
-      Card card;
-      is >> card;
-      if (wild && card == Card::JACK) {
-         card = Card::WILD;
-         jacks++;
+      Card card = parseCard<j>(is);
+      if (card == Card::JOKER) {
+         jokers++;
       } else {
          counts[card]++;
       }
@@ -48,14 +46,14 @@ template<bool wild> Hand<wild>::Hand(std::istream &is) {
    }
    for (auto &[_, count] : counts)
       ofkind[count]++;
-   for (int i = 4; jacks != 0 && i > 0; --i) {
+   for (int i = 4; jokers != 0 && i > 0; --i) {
       if (ofkind[i] == 0)
          continue;
       ofkind[i]--;
-      ofkind[i+jacks]++;
-      jacks = 0;
+      ofkind[i+jokers]++;
+      jokers = 0;
    }
-   ofkind[jacks]++;
+   ofkind[jokers]++;
    if (ofkind[5])
       type = HandType::FIVE;
    else if (ofkind[4])
@@ -71,16 +69,16 @@ template<bool wild> Hand<wild>::Hand(std::istream &is) {
    is >> bid;
 }
 
-template <bool wild> std::vector<Hand<wild>> parse(std::istream &is) {
-   std::vector<Hand<wild>> hands;
+template <Card j> std::vector<Hand<j>> parse(std::istream &is) {
+   std::vector<Hand<j>> hands;
    while (is)
       hands.emplace_back(is);
    hands.pop_back(); // kill the one added at EOF.
    return hands;
 };
 
-template <bool wild> void solve(std::istream &is, std::ostream &os) {
-   auto data = parse<wild>(is);
+template <Card j> void solve(std::istream &is, std::ostream &os) {
+   auto data = parse<j>(is);
    std::sort(data.begin(), data.end());
    ulong total = 0;
    for (size_t i = 0; i < data.size(); ++i)
@@ -88,5 +86,5 @@ template <bool wild> void solve(std::istream &is, std::ostream &os) {
    os << total << "\n";
 }
 
-void part1(std::istream &is, std::ostream &os) { solve<false>(is, os); }
-void part2(std::istream &is, std::ostream &os) { solve<true>(is, os); }
+void part1(std::istream &is, std::ostream &os) { solve<Card::JACK>(is, os); }
+void part2(std::istream &is, std::ostream &os) { solve<Card::JOKER>(is, os); }

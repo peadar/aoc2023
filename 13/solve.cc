5bb2fa8c7be94891ddd791fabaa532d2673e07f9
Peter Edwards <peadar@arista.com>
2023-12-13 21:24:34 +0000

    Part 2

--- 13/solve.cc
+++ 13/solve.cc
@@ -1,36 +1,43 @@
 #include "../aoc.h"
 #include <vector>
-#include <iterator>
 #include <unistd.h>
 #include <optional>
 #include <cassert>
 
 using BITS = uint64_t;
 
-struct GRID {
+template <bool Smudge> struct GRID {
    std::vector<BITS> data;
    size_t cols = 0;
    GRID rot90() const;
 
-   template <typename It>
-   std::optional<size_t> palindromeAt(It begin, It end) {
-      for (auto s = begin; s < end - 1; ++s) {
+   template <typename It> std::optional<size_t> findPalindrome(It begin, It end) {
+      auto evenbegin = (end - begin) % 2 == 0 ? begin : begin + 1;
+      for (auto s = evenbegin; s < end - 1; s += 2) {
+         auto matches = (end - s)/2;
+         assert((end - s) % 2 == 0);
+         int zeroes = 0;
+         int ones = 0;
          for (auto p = s, e = end;; ++p, --e) {
             if (p == e) {
-               return p - begin;
-            }
-            if (*p != e[-1])
+               if ( zeroes == matches - Smudge && ones == Smudge)
+                  return p - begin;
                break;
+            }
+            if (*p == e[-1])
+               zeroes++;
+            if (__builtin_popcount(*p ^ e[-1]) == 1)
+               ones++;
          }
       }
       return std::nullopt;
    }
 
    std::optional<size_t> palindromeAtNR() {
-      auto p = palindromeAt(data.begin(), data.end());
+      auto p = findPalindrome(data.begin(), data.end());
       if (p)
-         return *p;
-      p = palindromeAt(data.rbegin(), data.rend());
+         return p;
+      p = findPalindrome(data.rbegin(), data.rend());
       if (p)
          return data.size() - *p;
       return std::nullopt;
@@ -45,11 +52,11 @@ struct GRID {
       assert(p);
       return *p;
    }
+
 };
 
-GRID
-GRID::rot90() const {
-   GRID g;
+template <bool Smudge> GRID<Smudge> GRID<Smudge>::rot90() const {
+   GRID<Smudge> g;
    g.cols = data.size();
    g.data.resize(cols);
    for (BITS fromrow = 0; fromrow < data.size(); ++fromrow)
@@ -60,13 +67,12 @@ GRID::rot90() const {
    return g;
 }
 
-
-using GRIDS = std::vector<GRID>;
-
-GRIDS parse(std::istream &is) {
-   GRIDS result;
-   GRID cur;
-   for (std::string line;;) {
+template <bool Smudge> using GRIDS = std::vector<GRID<Smudge>>;
+template <bool Smudge> GRIDS<Smudge> parse(std::istream &is) {
+   GRIDS<Smudge> result;
+   GRID<Smudge> cur;
+   for (int lineNo = 1;; ++lineNo) {
+      std::string line;;
       std::getline(is, line);
       if (line == "") {
          result.push_back(std::move(cur));
@@ -84,11 +90,10 @@ GRIDS parse(std::istream &is) {
       }
       cur.data.push_back(b);
    }
-
    return result;
 }
 
-std::ostream &operator <<(std::ostream &os, const GRID &grid) {
+template <bool Smudge> std::ostream &operator <<(std::ostream &os, const GRID<Smudge> &grid) {
    for (auto datum : grid.data) {
       for (int i = 0; i < grid.cols; ++i)
          os << (datum & (1 << i )? '#':'.');
@@ -97,25 +102,18 @@ std::ostream &operator <<(std::ostream &os, const GRID &grid) {
    return os << "\n";
 }
 
-void part1(std::istream &is, std::ostream &os) {
-   auto day = parse(is);
+template <bool Smudge> unsigned long solve(std::istream &is, std::ostream &os) {
+   auto day = parse<Smudge>(is);
    unsigned long sum = 0;
-   for (auto &orig : day) {
-      auto val = orig.palindromeAt();
-      sum += val;
-   }
-   os << "part1: " << sum << "\n";
+   for (auto &orig : day)
+      sum += orig.palindromeAt();
+   return sum;
+}
+
+void part1(std::istream &is, std::ostream &os) {
+   os << "part1: " <<  solve<false>(is, os) << "\n";
 }
 
 void part2(std::istream &is, std::ostream &os) {
-   auto day = parse(is);
-   /*
-   for (auto &d : day) {
-      os << d;
-      os << "\n";
-      os << d.rot90();
-      os << "\n";
-      os << "\n";
-   }
-   */
+   os << "part2: " <<  solve<true>(is, os) << "\n";
 }

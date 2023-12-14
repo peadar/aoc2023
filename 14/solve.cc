388ea0530ee37a832513e89ff8e5f7c40639d87f
Peter Edwards <peadar@arista.com>
2023-12-14 10:44:20 +0000

    hopeless code for BF

--- 14/solve.cc
+++ 14/solve.cc
@@ -2,14 +2,50 @@
 #include <vector>
 #include <algorithm>
 
+
 struct Day {
    std::vector<std::vector<char>> grid;
    Day(std::istream &is);
-   
-   void northify();
+ 
+   template <typename Direction> void shuffle(const Direction &d);
    long tally();
-
+   void cycle();
+   auto operator <=>(const Day &) const = default;
 };
+
+struct North {
+   Day &d;
+   size_t majorsize() const noexcept { return d.grid.size(); }
+   size_t minorsize() const noexcept { return d.grid[0].size(); }
+   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[maj][min]; }
+   North(Day &d_) : d(d_) {}
+};
+
+struct South {
+   Day &d;
+   size_t majorsize() const noexcept { return d.grid.size(); }
+   size_t minorsize() const noexcept { return d.grid[0].size(); }
+   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[d.grid.size() - 1 - maj][min]; }
+   South(Day &d_) : d(d_) {}
+};
+
+struct West {
+   Day &d;
+   size_t majorsize() const noexcept { return d.grid[0].size(); }
+   size_t minorsize() const noexcept { return d.grid.size(); }
+   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[min][maj]; }
+   West(Day &d_) : d(d_) {}
+};
+
+struct East {
+   Day &d;
+   size_t majorsize() const noexcept { return d.grid[0].size(); }
+   size_t minorsize() const noexcept { return d.grid.size(); }
+   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[min][d.grid[0].size() - 1 - maj]; }
+   East(Day &d_) : d(d_) {}
+};
+
+
 
 Day::Day(std::istream &is) {
    for (std::string line; std::getline(is, line) && is; ) {
@@ -18,13 +54,14 @@ Day::Day(std::istream &is) {
    }
 }
 
+template <typename Direction>
 void
-Day::northify() {
-   for (size_t r = 1; r < grid.size(); ++r) {
-      for (size_t c = 0; c < grid[r].size(); ++c) {
-         if (grid[r][c] == 'O') {
-            for (size_t newr = r - 1; newr < grid.size() && grid[newr][c] == '.' ; --newr) {
-               std::swap(grid[newr][c], grid[newr+1][c]);
+Day::shuffle(const Direction &d) {
+   for (size_t r = 1; r < d.majorsize(); ++r) {
+      for (size_t c = 0; c < d.minorsize(); ++c) {
+         if (d(r, c) == 'O') {
+            for (size_t newr = r - 1; newr < d.majorsize() && d(newr, c) == '.' ; --newr) {
+               std::swap(d(newr,c), d(newr+1, c));
             }
          }
       }
@@ -51,13 +88,47 @@ Day::tally() {
    return result;
 }
 
+void Day::cycle() {
+   North n(*this);
+   South s(*this);
+   East e(*this);
+   West w(*this);
+   shuffle(n);
+   shuffle(w);
+   shuffle(s);
+   shuffle(e);
+}
+
+std::ostream &operator <<(std::ostream &os, const Day &day) {
+   for (auto &row : day.grid) {
+      for (auto &col : row) {
+         os << col;
+      }
+      os << "\n";
+   }
+   return os << "\n";
+}
+
+
 void part1(std::istream &is, std::ostream &os) {
    Day day { is };
-   day.northify();
+
+   North n(day);
+   day.shuffle(n);
    day.tally();
 
    os << "part1: " << day.tally() << "\n";
 }
 
 void part2(std::istream &is, std::ostream &os) {
+
+   Day day { is };
+   Day start = day;
+   for (int i = 0; i < 1000000000 ; ++i) {
+      day.cycle();
+      if (day == start) {
+         os << "period: " << i << "\n";
+         break;
+      }
+   }
 }

9371ceb2ad1c56467f7b41526cce826c5cfb50b8
Peter Edwards <peadar@arista.com>
2023-12-14 12:55:08 +0000

    Working part 2 and cleanup

--- 14/solve.cc
+++ 14/solve.cc
@@ -2,50 +2,45 @@
 #include <vector>
 #include <algorithm>
 
+namespace {
 
 struct Day {
    std::vector<std::vector<char>> grid;
    Day(std::istream &is);
- 
-   template <typename Direction> void shuffle(const Direction &d);
-   long tally();
-   void cycle();
+   template <typename Direction> Day &shuffle(const Direction &d) noexcept;
+   long tally() const noexcept;
+   Day &cycle() noexcept;
    auto operator <=>(const Day &) const = default;
 };
 
-struct North {
-   Day &d;
-   size_t majorsize() const noexcept { return d.grid.size(); }
-   size_t minorsize() const noexcept { return d.grid[0].size(); }
-   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[maj][min]; }
-   North(Day &d_) : d(d_) {}
+// There is definitely a more succinct way to do all this...
+// Share the same shuffle() code with classes to hide the details of how the
+// cardinal direction affects the indexing.
+struct Vertical {
+   size_t majorsize(Day &d) const noexcept { return d.grid.size(); }
+   size_t minorsize(Day &d) const noexcept { return d.grid[0].size(); }
 };
 
-struct South {
-   Day &d;
-   size_t majorsize() const noexcept { return d.grid.size(); }
-   size_t minorsize() const noexcept { return d.grid[0].size(); }
-   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[d.grid.size() - 1 - maj][min]; }
-   South(Day &d_) : d(d_) {}
+struct Horizontal {
+   size_t majorsize(Day &d) const noexcept { return d.grid[0].size(); }
+   size_t minorsize(Day &d) const noexcept { return d.grid.size(); }
 };
 
-struct West {
-   Day &d;
-   size_t majorsize() const noexcept { return d.grid[0].size(); }
-   size_t minorsize() const noexcept { return d.grid.size(); }
-   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[min][maj]; }
-   West(Day &d_) : d(d_) {}
+struct North : Vertical {
+   char &operator ()(Day &d, size_t maj, size_t min) const noexcept { return d.grid[maj][min]; }
 };
 
-struct East {
-   Day &d;
-   size_t majorsize() const noexcept { return d.grid[0].size(); }
-   size_t minorsize() const noexcept { return d.grid.size(); }
-   char &operator ()(size_t maj, size_t min) const noexcept { return d.grid[min][d.grid[0].size() - 1 - maj]; }
-   East(Day &d_) : d(d_) {}
+struct South : Vertical {
+   char &operator ()(Day &d, size_t maj, size_t min) const noexcept { return d.grid[d.grid.size() - 1 - maj][min]; }
 };
 
+struct West : Horizontal {
+   char &operator ()(Day &d, size_t maj, size_t min) const noexcept { return d.grid[min][maj]; }
+};
 
+struct East : Horizontal {
+   char &operator ()(Day &d, size_t maj, size_t min) const noexcept { return d.grid[min][d.grid[0].size() - 1 - maj]; }
+};
 
 Day::Day(std::istream &is) {
    for (std::string line; std::getline(is, line) && is; ) {
@@ -54,81 +49,50 @@ Day::Day(std::istream &is) {
    }
 }
 
-template <typename Direction>
-void
-Day::shuffle(const Direction &d) {
-   for (size_t r = 1; r < d.majorsize(); ++r) {
-      for (size_t c = 0; c < d.minorsize(); ++c) {
-         if (d(r, c) == 'O') {
-            for (size_t newr = r - 1; newr < d.majorsize() && d(newr, c) == '.' ; --newr) {
-               std::swap(d(newr,c), d(newr+1, c));
-            }
-         }
-      }
-   }
+template <typename Direction> Day & Day::shuffle(const Direction &d) noexcept {
+   for (size_t r = 1; r < d.majorsize(*this); ++r)
+      for (size_t c = 0; c < d.minorsize(*this); ++c)
+         if (d(*this, r, c) == 'O')
+            for (size_t newr = r - 1; newr < d.majorsize(*this) && d(*this, newr, c) == '.' ; --newr)
+               std::swap(d(*this, newr,c), d(*this, newr+1, c));
+   return *this;
 }
 
-long
-Day::tally() {
-   long weight = 1;
-   long result = 0;
-   for (auto row = grid.rbegin(); row != grid.rend(); ++row, ++weight) {
-      result += weight * std::count(row->begin(), row->end(), 'O');
-   }
-
-   for (size_t r = 1; r < grid.size(); ++r) {
-      for (size_t c = 0; c < grid[r].size(); ++c) {
-         if (grid[r][c] == 'O') {
-            for (size_t newr = r - 1; newr < grid.size() && grid[newr][c] == '.' ; --newr) {
-               std::swap(grid[newr][c], grid[newr+1][c]);
-            }
-         }
-      }
-   }
+long Day::tally() const noexcept {
+   long weight = grid.size(), result = 0;
+   for (auto row : grid)
+      result += weight-- * std::count(row.begin(), row.end(), 'O');
    return result;
 }
 
-void Day::cycle() {
-   North n(*this);
-   South s(*this);
-   East e(*this);
-   West w(*this);
-   shuffle(n);
-   shuffle(w);
-   shuffle(s);
-   shuffle(e);
+Day & Day::cycle() noexcept {
+   return shuffle(North{}).shuffle(West{}).shuffle(South{}).shuffle(East{});
 }
 
-std::ostream &operator <<(std::ostream &os, const Day &day) {
-   for (auto &row : day.grid) {
-      for (auto &col : row) {
-         os << col;
-      }
-      os << "\n";
-   }
-   return os << "\n";
 }
-
 
 void part1(std::istream &is, std::ostream &os) {
-   Day day { is };
-
-   North n(day);
-   day.shuffle(n);
-   day.tally();
-
-   os << "part1: " << day.tally() << "\n";
+   os << "part1: " << Day{is}.shuffle(North{}).tally() << "\n";
 }
 
 void part2(std::istream &is, std::ostream &os) {
+   Day day { is }, old { day };
+   long count, period = 0;
+   constexpr long target = 1000000000;
 
-   Day day { is };
-   Day start = day;
-   for (int i = 0; i < 1000000000 ; ++i) {
+   // First find a cycle by advancing day twice as fast as old
+   for (count = 1, day.cycle(); day != old;++count) {
       day.cycle();
-      if (day == start) {
-         os << "period: " << i << "\n";
-         break;
-      }
+      if (count % 2 == 0)
+         old.cycle();
    }
+   // Keep counting until we find the pattern repeats again, to get the cycle length.
+   do {
+      day.cycle();
+      period++;
+   } while (day != old);
+   // Now run the remaining iterations until it's its as if we ran to the target.
+   for (int i = 0; i < (target - count) % period; ++i)
+      day.cycle();
+   os << "part2: " << day.tally() << "\n";
 }

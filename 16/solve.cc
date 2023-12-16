e54a49494f5e09963e152686918ac45a2019caf2
Peter Edwards <peadar@arista.com>
2023-12-16 18:16:13 +0000

    golf

--- 16/solve.cc
+++ 16/solve.cc
@@ -3,19 +3,7 @@
 #include <algorithm>
 
 namespace {
-struct Location {
-   int row, col;
-   Location &operator += (Location rhs) {
-      row += rhs.row;
-      col += rhs.col;
-      return *this;
-   }
-   Location operator + (Location d) const {
-      Location neu = *this;
-      neu += d;
-      return neu;
-   }
-};
+using Location = std::pair<int, int>;
 
 struct Tile {
    char content, directions; // one bit each for NSEW.
@@ -24,62 +12,58 @@ struct Tile {
 
 struct Grid {
    std::vector<std::vector<Tile>> d;
-   Tile &operator[](Location l) { return d[l.row][l.col]; }
    unsigned long energy() {
       unsigned long total = 0;
-      for (auto row : d)
-         for (const auto & col : row)
-            if (col.directions != 0)
+      for (auto &row : d)
+         for (auto & col : row)
+            if (col.directions != 0) {
                ++total;
+               col.directions = 0;
+            }
       return total;
    }
    int rowcount() { return d.size(); }
    int colcount() { return d[0].size(); }
    bool contains(Location l) {
-      return l.row >= 0 && l.row < rowcount() && l.col >= 0 && l.col < colcount();
+      return l.first >= 0 &&
+         l.first < rowcount() &&
+         l.second >= 0 && l.second < colcount();
    }
-   void clear() {
-      for (auto &row : d)
-         for (auto &tile : row)
-            tile.directions = 0;
-   }
-   void cast(Location l, Location d);
+   Grid & cast(Location l, Location d);
    Grid(std::istream &);
 };
 
-void Grid::cast(Location l, Location d) {
-   auto dirbit = [](const Location &l)
-      { return l.row == -1 ? 0 : l.row == 1 ? 1 : l.col == -1 ? 2 : 3; };
-   int pos = dirbit(d);
+Grid &Grid::cast(Location l, Location dir) {
    for (;;) {
-      l += d;
+      int pos = dir.first == -1 ? 0 : dir.first == 1 ? 1 : dir.second == -1 ? 2 : 3;
+      l.first += dir.first;
+      l.second += dir.second;
       if (!contains(l))
-         return;
-      Tile &tile = (*this)[l];
+         return *this;
+      Tile &tile = d[l.first][l.second];
       if (tile.directions & (1<<pos))
-         return;
+         return *this;
       tile.directions |= (1 << pos);
       switch (tile.content) {
          case  '/':
-            d.row = - d.row;
-            d.col = - d.col;
+            dir.first = - dir.first;
+            dir.second = - dir.second;
             /* fallthrough */
          case '\\':
-            std::swap(d.row, d.col);
-            pos = dirbit(d);
+            std::swap(dir.first, dir.second);
             break;
          case '|':
-            if (d.col) {
+            if (dir.second) {
                cast(l, {-1, 0});
                cast(l, {1, 0});
-               return;
+               return *this;
             }
             break;
          case '-':
-            if (d.row) {
+            if (dir.first) {
                cast(l, {0, 1});
                cast(l, {0, -1});
-               return;
+               return *this;
             }
             break;
          default:
@@ -97,9 +81,7 @@ Grid::Grid(std::istream &is) {
 }
 
 void part1(std::istream &is, std::ostream &os) {
-   Grid g{is};
-   g.cast({0, -1}, {0, 1});
-   os << "part 1: " << g.energy() << "\n";
+   os << "part 1: " << std::move(Grid{is}).cast({0, -1}, {0, 1}).energy() << "\n";
 }
 
 void part2(std::istream &is, std::ostream &os) {
@@ -108,7 +90,6 @@ void part2(std::istream &is, std::ostream &os) {
    auto dopos = [&](Location start, Location d) {
       g.cast(start, d);
       maxenergy = std::max(maxenergy, g.energy());
-      g.clear();
    };
    for (int row = 0; row < g.rowcount();  ++row) {
       dopos({row, -1}, {0, 1});
